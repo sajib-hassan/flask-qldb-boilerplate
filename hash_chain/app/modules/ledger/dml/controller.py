8c00e2d9e58863c86fd6cc4de774011da857d12a
Rifatul Islam Chayon <rifatulchayon@gmail.com>
2020-06-08 16:25:02 +0600

    modified insert document api.

--- hash_chain/app/modules/ledger/dml/controller.py
+++ hash_chain/app/modules/ledger/dml/controller.py
@@ -13,13 +13,13 @@ api = DmlDto.api
 table_name = table_name_parser_plain()
 
 
-@api.route('/insert_documents')
+@api.route('/documents')
 @api.doc(security="jwt_token")
 @api.response(int(HTTPStatus.UNAUTHORIZED), "Token is invalid or expired.")
 @api.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
 @api.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "Internal server error.")
-class LedgerTableIndex(Resource):
-    """Handles HTTP requests to URL: /api/v1/ledger/dml/insert_documents."""
+class LedgerTableDocument(Resource):
+    """Handles HTTP requests to URL: /api/v1/ledger/dml/documents."""
 
     @api.doc('Insert documents into a table in a QLDB ledger.')
     @api.expect(table_name, validate=True)
@@ -30,3 +30,12 @@ class LedgerTableIndex(Resource):
         """Insert documents into a table in a QLDB ledger."""
         args = table_name.parse_args(req=None, strict=False)
         return DmlServices.do_insert_documents(**args)
+
+    @api.doc('Get documents from a table in a QLDB ledger.')
+    @api.expect(table_name, validate=True)
+    @admin_token_required
+    @api.response(int(HTTPStatus.CREATED), "Documents retrieved successfully.")
+    @api.response(int(HTTPStatus.UNPROCESSABLE_ENTITY), "Unable to retrieve documents.")
+    def get(self):
+        """Retrieve documents from a table in a QLDB ledger."""
+        pass
